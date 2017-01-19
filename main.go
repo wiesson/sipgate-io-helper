87da377da9e2341f71e579788a43582cc5f05dee
Arne Wiese <wiese@sipgate.de>
2017-01-19 17:14:58 +0100

    Add command-line flags

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strings"
+	"flag"
+	"net/url"
+	"strconv"
 )
+
+const devUrl string = "https://api.dev.sipgate.com/login/sipgate-apps/protocol/openid-connect/token"
+const liveUrl string = "https://api.sipgate.com/login/sipgate-apps/protocol/openid-connect/token"
 
 type PushApiResponse struct {
 	Answer string `xml:"onAnswer,attr"`
@@ -31,16 +36,26 @@ type PushApiUrls struct {
 }
 
 type API struct {
-	user       string
-	password   string
-	Token      string
-	PushApiUrl string
+	user        string
+	password    string
+	AccessToken string
+	PushApiUrl  string
+	Env         string
 }
 
 func (a *API) GetSipgateApiToken() string {
-	b := new(bytes.Buffer)
-	json.NewEncoder(b).Encode(map[string]string{"username": a.user, "password": a.password})
-	res, err := http.Post("https://api.sipgate.com/v1/authorization/token", "application/json; charset=utf-8", b)
+	payload := url.Values{}
+	payload.Set("client_id", "sipgate-app-web")
+	payload.Add("username", a.user)
+	payload.Add("password", a.password)
+	payload.Add("grant_type", "password")
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", liveUrl, bytes.NewBufferString(payload.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(payload.Encode())))
+
+	res, err := client.Do(req)
 
 	if err != nil {
 		panic(err)
@@ -50,16 +65,18 @@ func (a *API) GetSipgateApiToken() string {
 		panic("Got wrong status code during token request")
 	}
 
-	var body struct {
-		Token string `json:"Token"`
+	var responseBody struct {
+		AccessToken   string `json:"access_token"`
+		refreshToken  string `json:"refresh_token"`
+		identityToken string `json:"identity_token"`
 	}
 
-	json.NewDecoder(res.Body).Decode(&body)
-	a.Token = body.Token
+	json.NewDecoder(res.Body).Decode(&responseBody)
+	a.AccessToken = responseBody.AccessToken
 
-	fmt.Println("Got Token from sipgate api: ", a.Token)
+	fmt.Println("Got AccessToken from sipgate api: ", a.AccessToken)
 
-	return a.Token
+	return a.AccessToken
 }
 
 func (a *API) GetNgrokUrl() {
@@ -92,7 +109,7 @@ func (a *API) SetPushApiUrl() {
 	json.NewEncoder(b).Encode(map[string]string{"incomingUrl": a.PushApiUrl, "outgoingUrl": a.PushApiUrl})
 
 	req, _ := http.NewRequest("PUT", "https://api.sipgate.com/v1/settings/sipgateio", b)
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.AccessToken))
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	client := &http.Client{}
@@ -122,10 +139,28 @@ func (a *API) pushApiResponseHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	api := &API{user:os.Args[1], password:os.Args[2]}
+	email := flag.String("email", "username", "Your sipgate email address")
+	password := flag.String("password", "password", "The password for your sipgate account")
+	env := flag.String("env", "dev", "dev, live")
+
+	flag.Parse()
+
+	if *email == "username" {
+		fmt.Println("Please enter your username")
+		return
+	}
+
+	if *password == "password" {
+		fmt.Println("Please enter your password")
+		return
+	}
+
+	api := &API{user:*email, password:*password, Env:*env}
+	api.GetNgrokUrl()
 	api.GetSipgateApiToken()
 	api.SetPushApiUrl()
-	api.GetNgrokUrl()
+
+	fmt.Print("\n\n")
 
 	http.HandleFunc("/", api.pushApiResponseHandler)
 	http.ListenAndServe(":3000", nil)
